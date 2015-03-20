8ea9fd5929f95ed31ebd131cdaf3768f5462909b
Paul Handly <ph@betaworks.com>
2015-03-20 13:38:18 -0400

    Bump to 0.1.0.dev1, prepare for release

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ class Tox(test):
 
 setup(
     name='boosh',
-    version='0.0.2',
+    version='0.1.0.dev1',
     packages=find_packages(),
     install_requires=[
         'botocore>=0.94.0',
@@ -26,7 +26,14 @@ setup(
         ],
     },
     tests_require=['tox'],
-    cmdclass={"test": Tox},
-    author="Paul Handly",
-    author_email="ph@betaworks.com",
+    cmdclass={'test': Tox},
+    author='Paul Handly',
+    author_email='ph@betaworks.com',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+    ],
 )
